8bd5e78a545f746f35a6a95694f913fc1c636d15
Thyago Barbosa <thyagobr@gmail.com>
2014-04-09 19:49:57 -0300

    First blood!

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var socketio = require('socket.io');
+var validator = require('validator');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendfile('views/index.html');
+});
+
+var server = app.listen(7007, function() {
+  console.log("Listening on port %d", server.address().port);
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection', function(socket) {
+  console.log("Connection!");
+  socket.emit('mastermind', '!gralk: bem-vindo ao gralk!');
+  socket.emit('mastermind', '!gralk: escolha seu apelido usando: /nick <strong>apelido</strong>');
+  socket.emit('mastermind', '!grak: exemplo: /nick thyagobr');
+  socket.emit('mastermind', '!grak: divirta-se!');
+  socket.nickname = "RandomDude" + Math.floor(Math.random()*9999);
+  socket.on('message', function(data) {
+    data = validator.escape(data);
+    console.log(socket.id + ": " + data);
+    tokenize = data.split(" ");
+    if (tokenize[0] == "/nick") {
+      console.log("NICKNAME: " + tokenize[1]);
+      if (tokenize[1].length > 10) {
+        socket.emit('message', '!gralk: Tamanho máximo do nickname: 10 chars');
+        return;
+      }
+      io.sockets.emit('message', '!gralk: ' + socket.nickname + ' agora é: ' + tokenize[1]); 
+      socket.nickname = tokenize[1];
+      return;
+    }
+    io.sockets.emit('message', socket.nickname + ": " + data);
+  });
+});
+
